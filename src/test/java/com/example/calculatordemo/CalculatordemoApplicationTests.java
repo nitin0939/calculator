7c4118fd090b2434dea150f23526c1dbd4b76246
Nitin Kumar <nitink2@KENTMAIL.GRP>
2020-08-04 11:11:03 +0530

    maven project

---
+++ src/test/java/com/example/calculatordemo/CalculatordemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calculatordemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatordemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
